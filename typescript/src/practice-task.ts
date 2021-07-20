8cc5fea75dec9bb718caa245080e78df120f0303
convict-git <official.mr.convict@gmail.com>
2021-07-20 18:15:20 +0530

    Added testcases

--- typescript/src/practice-task.ts
+++ typescript/src/practice-task.ts
@@ -1,7 +1,7 @@
 const pluck =
-   <T, K extends keyof T, S extends T>(key: K, ...objList: S[]): S[K][] => {
+   <T, K extends keyof T>(key: K, ...objList: T[]): T[K][] => {
       return objList.reduce(
-         (prevRes: S[K][], obj: S) => {
+         (prevRes: T[K][], obj: T) => {
             if (typeof obj[key] !== 'undefined') {
                prevRes.push(obj[key]);
             }
@@ -10,10 +10,34 @@ const pluck =
       );
    }
 
+// const pluck =
+//    <T, K extends keyof T, S extends T>(key: K, ...objList: S[]): S[K][] => {
+//       return objList.reduce(
+//          (prevRes: S[K][], obj: S) => {
+//             if (typeof obj[key] !== 'undefined') {
+//                prevRes.push(obj[key]);
+//             }
+//             return prevRes;
+//          }, []
+//       );
+//    }
+
+interface Point {
+   x: number; y: number; label: string
+}
+const myPoint_A: Point = { x: 1, y: 2, label: "start" };
+const myPoint_B: Point = { x: 2, y: 3, label: "mid" };
+const myPoint_C: Point = { x: 3, y: 4, label: "end" };
+
+console.log(pluck('label', myPoint_A, myPoint_B, myPoint_C));
+// [ 'start', 'mid', 'end' ]
+
+// --------------------------------------------------------------
 const getValues = <T, K extends keyof T>(obj: T): T[K][] => {
-   return Object.keys(obj).map(key => obj[key]);
+   return Object.keys(obj).map(key => obj[(key as K)]);
 }
 
+// --------------------------------------------------------------
 const flatten = (arArg: any[], depth: number = 1): any[] => {
    return arArg.reduce(
       (prevRes: any[], elem: any) => {
@@ -28,6 +52,12 @@ const flatten = (arArg: any[], depth: number = 1): any[] => {
    );
 }
 
+console.log(flatten([1, 2, [3, 4, [5, 6, [7, 8]]]], 1));
+// [ 1, 2, 3, 4, [ 5, 6, [ 7, 8 ] ] ]
+console.log(flatten([1, 2, [3, 4, [5, 6, [7, 8]]]], 2));
+// [ 1, 2, 3, 4, 5, 6, [ 7, 8 ] ]
+
+// --------------------------------------------------------------
 // f, g, h (a) -> f(g(h(a)))  - conventianal
 // f, g, h (a) -> h(g(f(a))) - what we want ?
 const revComp = (arg: any, ...f: ((_: any) => any)[]): any => {
@@ -36,3 +66,4 @@ const revComp = (arg: any, ...f: ((_: any) => any)[]): any => {
    }, arg
    );
 }
+// --------------------------------------------------------------
